9a618f1cc2114cdd492a5bd3627d7604d19f66a7
cwild10 <cwild@student.fullsail.edu>
2022-06-24 23:15:38 -0400

    Command

---
+++ IBaseCommand.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "CalculatorProcessor.h"
+class IBaseCommand: CalculatorProcessor
+{
+public:
+
+	 virtual ~IBaseCommand() = 0;
+	 switch (pro)
+		 {
+			 case '+'
+				 cout << "AddCommand" << 
+				 break;
+				 case '-'
+					 cout << "SUbtractCommand" <<
+					 break;
+					 case '*'
+						 cout << "MultiplyCommand" <<
+						 break;
+						 case '/'
+							 cout << "DivideCommand" <<
+							 break;
+		 }
+};
+
